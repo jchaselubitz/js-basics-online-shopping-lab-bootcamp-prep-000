e6ee7271926c5ae670bcb95765c3abb16fb38e10
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-19 18:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,8 +65,7 @@ function search(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
         console.log(cart[i])
-        var Num = 0
-        Num = cart[i]
+        var Num = cart[i]
     }
   } return Num
 }
