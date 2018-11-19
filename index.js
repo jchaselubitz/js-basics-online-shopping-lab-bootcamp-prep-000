7427b7052e49e691a526648cdb76169fcc371c75
remarkable-workshop-2804 <jchaselubitz@gmail.com>
2018-11-19 19:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,9 +65,8 @@ function search(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
         Num = i
-        } else {
-          return "That item is not in your cart."
-        }
+    
+      }
   } return Num
 }
 console.log(`this is search ${search()}`)
